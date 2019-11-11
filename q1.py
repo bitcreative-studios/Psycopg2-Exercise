7cd10fa4afffde6da7c9459075a3b1a535a66718
KatrinaaDing <xiaoding1998@hotmail.com>
2019-11-11 14:53:25 +1100

    fi q1

---
+++ q1.py
@@ -0,0 +1,26 @@
+import re
+
+import ass3
+conn = ass3.connect()
+
+cur = conn.cursor()
+cur.execute(
+    """
+    SELECT s.code, c.quota, count(ce.person_id)
+	From Courses c
+	JOIN Subjects s on c.subject_id = s.id
+	JOIN Course_Enrolments ce on c.id = ce.course_id
+	WHERE c.quota > 50 and c.term_id = 5199 
+	GROUP BY c.id,course_id, s.code
+	ORDER BY s.code
+	"""
+)
+
+for tup in cur.fetchall():
+	if (tup[2] > tup[1]):
+		percentage = round((tup[2]/tup[1])*100)
+		print("{} {}%".format(tup[0], percentage))
+
+
+cur.close()
+conn.close()
